fc5807e3847788a3428bacca9da1735fe6aa4dad
Pablo Formoso Estrada <pablo@pabloformoso.com>
2012-10-20 11:28:16 +0200

    first commit web app

---
+++ CarPoolWeb/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarPoolWeb::Application.config.secret_token = 'a76290e83127df6988cba95dd4d93bb2304bfc85cb58cf62b7710f8cc26e8f2ad876962b67ecb09e76125121154359718ade19b2d3be3924c36b1f0c10978c9f'
